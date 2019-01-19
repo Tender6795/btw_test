c7f6c1bfb037aaccdc1f5b7174b1ba2910af15ec
Tender6795 <39260215+Tender6795@users.noreply.github.com>
2019-01-19 16:59:02 +0200

    FirstCommit

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php
+$host="127.0.0.1";
+$usernameDb="root";
+$passwordDb="";
+$dbName="bwtTest";
+
+$dbUser="users";
+$dbUserFirstname="firstname";
+$dbUserLastname="lastname";
+$dbUserEmail="email";
+$dbUserPassword="password";
+$dbUserDate="date";
+$dbUserGender="gender";
+
